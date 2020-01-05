39f98df4b5442d07742b90df4d2be0cf2e17389a
Luca <sarius009@gmail.com>
2020-01-05 16:34:27 +0100

    Use body-parser

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import logger from './middlewares/logger';
 
 const server = express();
 
+server.use(bodyParser.json());
 server.use(logger);
 
 server.get('/', (req: express.Request, res: express.Response) => {
